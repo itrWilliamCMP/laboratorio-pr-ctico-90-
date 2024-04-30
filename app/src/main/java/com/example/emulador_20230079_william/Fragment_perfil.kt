ba12bb24842fab40b3ba3ffdd9e200242ffe5484
William Campos <20230079@ricaldone.edu.sv>
2024-04-29 21:40:29 -0600

    Se termino

---
+++ app/src/main/java/com/example/emulador_20230079_william/Fragment_perfil.kt
@@ -0,0 +1,50 @@
+package com.example.emulador_20230079_william
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class Fragment_perfil : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_contactanos, container, false)
+
+        val button: Button = view.findViewById(R.id.btnContactanos)
+        button.setOnClickListener {
+            Toast.makeText(context, "William", Toast.LENGTH_SHORT).show()
+        }
+
+        return view
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Fragment_contactanos().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/example/emulador_20230079_william/activity_login.kt
@@ -0,0 +1,36 @@
+package com.example.emulador_20230079_william
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class activity_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+
+        btnLogin.setOnClickListener {
+
+            //Navegaciòn emtre pantallas con Intent
+            val Login = Intent(this, MainActivity::class.java)
+            startActivity(Login)
+
+            }
+
+
+
+        }
+}
